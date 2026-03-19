031ef1ba2a9bd5df666c2c241d6ba8a2e7eb3d23
margo-jpg <boickomargo@yandex.ru>
2026-03-19 15:26:13 +0700

    Update ResponsesStatus.cs

--- ResponsesStatus.cs
+++ ResponsesStatus.cs
@@ -1,10 +1,1 @@
-﻿namespace MasterFinder.Domain.Enums
-{
-    public enum ResponsesStatus
-    {
-        Pending = 1,    // ожидает решения заказчика
-        Accepted = 2,   // принят заказчиком
-        Rejected = 3,   // отклонен заказчиком
-        Withdrawn = 4   // отозван исполнителем
-    }
-}
+
