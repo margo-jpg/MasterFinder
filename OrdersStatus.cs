3f85b402a4811d586d2bfa405fc3d0430872577f
margo-jpg <boickomargo@yandex.ru>
2026-03-19 15:21:51 +0700

    Add files via upload

---
+++ OrdersStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterFinder.Domain.Enums
+{
+    public enum OrdersStatus
+    {
+        Open = 1,        // открыт для откликов
+        InProgress = 2,  // выполняется
+        Completed = 3,   // выполнен
+        Cancelled = 4    // отменен
+    }
+}
