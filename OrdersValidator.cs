f3a3e3a387a17835e60cbddb1bc9b6b97dd048bc
margo-jpg <boickomargo@yandex.ru>
2026-04-02 15:12:20 +0700

    Add files via upload

---
+++ OrdersValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MasterFinder.Domain.Entities;
+
+namespace MasterFinder.Domain.Validators
+{
+    public class OrdersValidator : AbstractValidator<Order>
+    {
+        public OrdersValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Название заказа обязательно")
+                .MinimumLength(3).WithMessage("Название должно содержать минимум 3 символа")
+                .MaximumLength(200).WithMessage("Название не может быть длиннее 200 символов");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(5000).WithMessage("Описание не может быть длиннее 5000 символов");
+
+            RuleFor(x => x.CustomerId)
+                .GreaterThan(0).WithMessage("ID заказчика должен быть положительным");
+        }
+    }
+}
---
+++ ResponsesValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MasterFinder.Domain.Entities;
+
+namespace MasterFinder.Domain.Validators
+{
+    public class ResponsesValidator : AbstractValidator<Response>
+    {
+        public ResponsesValidator()
+        {
+            RuleFor(x => x.OrderId)
+                .GreaterThan(0).WithMessage("ID заказа должен быть положительным");
+
+            RuleFor(x => x.ExecutorId)
+                .GreaterThan(0).WithMessage("ID исполнителя должен быть положительным");
+
+            RuleFor(x => x.Comment)
+                .MaximumLength(1000).WithMessage("Комментарий не может быть длиннее 1000 символов");
+        }
+    }
+}
